f8a3c9a14c984a7ce3e1f4fb5264fab8f36be771
LittleCoder <i7meavnktqegm1b@qq.com>
2016-03-05 13:25:02 +0800

    vote plugin is easy to use now

--- ItChat.py
+++ ItChat.py
@@ -1,6 +1,6 @@
 #coding=utf8
 import time
-import itchat.storage, itchat.out, itchat.argparser, robot
+import itchat.storage, itchat.out, itchat.argparser, itchat.robot
 from itchat.client import WeChatClient
 from plugin.ChatLikeCMD import ChatLikeCMD
 
@@ -11,7 +11,7 @@ def demo_robot(s, msgList, client): # ONLY FOR DEMO
     while True: 
         if msgList: 
             msg = msgList.pop()
-            if s.find_nickname(msg['FromUserName']): robot.deal_with_msg(msg, s, client)
+            if s.find_nickname(msg['FromUserName']): itchat.robot.deal_with_msg(msg, s, client)
         time.sleep(.1)
 if __name__ == '__main__':
     client_s = itchat.storage.Storage()
--- plugin/msgdealers/vote.py
+++ plugin/msgdealers/vote.py
@@ -1,5 +1,19 @@
-import re
+#coding=utf8
+import re, json, time
 from plugin.Sqlite3Client import Sqlite3Client
+
+VOTE_KEYWORD = 'vote'
+
+def load_vote_list():
+    try:
+        with open('vote.json') as f: voteList = json.loads(f.read())
+        if not voteList: raise Exception
+        for voteItem in voteList: # test json format
+            if not (voteItem.has_key('name') and voteItem.has_key('candidates')): raise Exception
+            if not (isinstance(voteItem['candidates'], list) and voteItem['candidates']): raise Exception
+        return voteList
+    except:
+        print 'There is something wrong with vote.json'
 
 def get_status(storageClass, userName):
     r = storageClass.get_other(userName)
@@ -10,28 +24,30 @@ def store_status(storageClass, userName, other):
     storageClass.update_user(PYQuanPin, Other = storageClass.get_str_of_other(other))
 
 def vote(storageClass, userName, msg):
+    if not VOTE_KEYWORD in msg: return False
     # key in sqlite3->other: vote
     # value in sqlite3->other: -1 for voted, 0 for waiting for vote, 1 for not voted
-    regex = re.compile('.*vote.* num ([0-9]+)')
-    r = re.findall(regex, msg)
-    if not r: return False
+    # values should be strings
+    voteList = load_vote_list()
+    if voteList is None: return False
     status = get_status(storageClass, userName)
-    if not status.has_key('vote'): status['vote'] = '1'
-    if status['vote'] == '-1': return 'You have voted'
-    with Sqlite3Client(storageClass.sqlDir) as s3c:
-        s3c.execute('create table if not exists vote (a text, b text, c text)')
-        q = s3c.query('select * from vote')
-        if not q:
-            s3c.insert_data('vote', items = ['0','0','0'])
-            q = s3c.query('select * from vote')
-        if r[0] in ('1','2','3'):
-            s3c.execute('delete from vote')
-            index = int(r[0]) - 1
-            insert_data = list(q[0])
-            insert_data[index] = str(int(insert_data[index]) + 1)
-            s3c.insert_data('vote', items = insert_data)
-            status['vote'] = '-1'
-            store_status(storageClass, userName, status)
-            return 'Vote for %s successfully'%r[0]
-        else:
-            return 'There is no candidate %s'%r[0]
+    for voteItem in voteList:
+        if not voteItem['name'] in msg: continue
+        if status.has_key('vote@' + voteItem['name']) and status['vote@' + voteItem['name']] == '-1':
+            return 'You have voted in ' + voteItem['name']
+        candidate = None
+        for c in voteItem['candidates']:
+            # bug will occur if one candidates's name contains another's
+            if c in msg: candidate = c;break
+        if candidate is None: return 'Candidates contains: ' + ', '.join(voteItem['candidates'])
+        # update vote database
+        with Sqlite3Client(storageClass.sqlDir) as s3c:
+            s3c.execute('create table if not exists vote (name text, candidate text, PYQuanPin text, time text)')
+            PYQuanPin = storageClass.find_PYQuanPin(userName)
+            s3c.insert_data('vote', items = [voteItem['name'], candidate, PYQuanPin, int(time.time())])
+        # update user status
+        status['vote@' + voteItem['name']] = '-1'
+        store_status(storageClass, userName, status)
+        return 'Vote for %s successfully'%candidate
+        break
+    return 'There are votes going on: ' + ', '.join([voteItem['name'] for voteItem in voteList])
