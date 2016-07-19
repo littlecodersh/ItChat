0460c338e07485d8698668e47b43a1fc2a42bd6c
tempdban <temp@dban.tk>
2016-07-20 04:01:04 +0800

    全部查询消耗太大

--- itchat/client.py
+++ itchat/client.py
@@ -151,7 +151,13 @@ class client(object):
                 'UserName': userName,
                 'ChatRoomId': '', }], }
         post_result = self.s.post(url, data = json.dumps(payloads), headers = headers)
-        result = tools.emojiRecover(post_result.content.decode('utf8', 'replace'))
+        result = post_result.content.decode('utf8', 'replace')
+        for contact in j["MemberList"]:
+            '''兄弟，函数如果传的是可变对象，传的就是引用，直接对对象修改就行。没必要写成
+        dic['User'] = tools.emoji_dealer(dic['User'])
+        其他的我也懒得改了。。。。。
+        '''
+            tools.emoji_dealer(contact)
         j = json.loads(result)['ContactList'][0]
         j['isAdmin'] = j['OwnerUin'] == int(self.loginInfo['wxuin'])
         return j
