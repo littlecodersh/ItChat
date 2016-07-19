541f9fdb6c177aba6a0273757bc3c69d702850e8
tempdban <temp@dban.tk>
2016-07-20 02:42:23 +0800

    新的emoji处理逻辑

--- itchat/tools.py
+++ itchat/tools.py
@@ -4,10 +4,17 @@ from . import config
 
 def clear_screen():
     os.system('cls' if config.OS == 'Windows' else 'clear')
+    
+def emojiRecover(string):
+    def match(matched):
+        return eval("u'\\U000{}'".format(matched.group("emoji")))
+
+    reobj = re.compile('<span class="emoji emoji(?P<emoji>.{,10})"></span>')
+    result, count = reobj.subn(match, string)
+    return result
+    
 def emoji_dealer(d):
-    regex = re.compile('^(.*?)(?:<span class="emoji (.*?)"></span>(.*?))+$')
-    match = re.findall(regex, d['NickName'])
-    if len(match) > 0: d['NickName'] = ''.join(match[0])
+    d['NickName'] = emojiRecover(d['NickName'])
     return d
 def check_file(fileDir):
     try:
