8913299d5e1437d437c78710b3dcf37464f9911e
LittleCoder <i7meavnktqegm1b@qq.com>
2016-07-30 07:39:05 +0800

    Fix emoji bug caused by wechat backstage

--- itchat/tools.py
+++ itchat/tools.py
@@ -7,7 +7,7 @@ except ImportError:
 
 from . import config
 
-emojiRegex = re.compile(r'<span class="emoji emoji(.*?)"></span>')
+emojiRegex = re.compile(r'<span class="emoji emoji(.{1,10})"></span>')
 htmlParser = HTMLParser()
 try:
     b = u'\u2588'
@@ -21,8 +21,18 @@ else:
 def clear_screen():
     os.system('cls' if config.OS == 'Windows' else 'clear')
 def emoji_formatter(d, k):
-    # there's still a serious bug about emoji match caused by wechat backstage
+    # _emoji_deebugger is for bugs about emoji match caused by wechat backstage
     # like :face with tears of joy: will be replaced with :cat face with tears of joy:
+    def _emoji_debugger(d, k):
+        s = d[k].replace('<span class="emoji emoji1f450"></span',
+            '<span class="emoji emoji1f450"></span>') # fix missing bug
+        def __fix_miss_match(m):
+            return '<span class="emoji emoji%s"></span>' % ({
+                '1f63c': '1f601', '1f639': '1f602', '1f63a': '1f603',
+                '1f4ab': '1f616', '1f64d': '1f614', '1f63b': '1f60d',
+                '1f63d': '1f618', '1f64e': '1f621', '1f63f': '1f622',
+                }.get(m.group(1), m.group(1)))
+        return emojiRegex.sub(__fix_miss_match, s)
     def _emoji_formatter(m):
         s = m.group(1)
         if len(s) == 6:
@@ -34,6 +44,7 @@ def emoji_formatter(d, k):
         else:
             return ('\\U%s'%m.group(1).rjust(8, '0')
                 ).encode('utf8').decode('unicode-escape', 'replace')
+    d[k] = _emoji_debugger(d, k)
     d[k] = emojiRegex.sub(_emoji_formatter, d[k])
 def msg_formatter(d, k):
     emoji_formatter(d, k)
@@ -80,4 +91,5 @@ try:
 except ImportError:
     def print_cmd_qr(fileDir, size = 37, padding = 3,
             white = BLOCK, black = '  '):
+        print('pillow should be installed to use command line QRCode: pip install pillow')
         print_qr(fileDir)
