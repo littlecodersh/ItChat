35ca39cae75c232cce2dc530d045ddce485d2c69
ZhangChaoqi <prncoprs@163.com>
2017-02-24 12:20:44 +0800

    modified function comments

--- itchat/core.py
+++ itchat/core.py
@@ -54,7 +54,7 @@ class Core(object):
             it is defined in components/login.py
             and of course every single move in login can be called outside
                 - you may scan source code to see how
-                - and modified according to your own demond
+                - and modified according to your own demand
         '''
         raise NotImplementedError()
     def get_QRuuid(self):
