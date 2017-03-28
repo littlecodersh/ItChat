d8ad0039c088cb2c67dd17a6f6e5feb094a2b60b
Chyroc <chen_yunpeng@foxmail.com>
2017-03-28 14:08:18 +0800

    add gitignore

--- itchat/utils.py
+++ itchat/utils.py
@@ -23,50 +23,59 @@ else:
     BLOCK = b
 friendInfoTemplate = {}
 for k in ('UserName', 'City', 'DisplayName', 'PYQuanPin', 'RemarkPYInitial', 'Province',
-        'KeyWord', 'RemarkName', 'PYInitial', 'EncryChatRoomId', 'Alias', 'Signature', 
-        'NickName', 'RemarkPYQuanPin', 'HeadImgUrl'):
+          'KeyWord', 'RemarkName', 'PYInitial', 'EncryChatRoomId', 'Alias', 'Signature',
+          'NickName', 'RemarkPYQuanPin', 'HeadImgUrl'):
     friendInfoTemplate[k] = ''
 for k in ('UniFriend', 'Sex', 'AppAccountFlag', 'VerifyFlag', 'ChatRoomId', 'HideInputBarFlag',
-        'AttrStatus', 'SnsFlag', 'MemberCount', 'OwnerUin', 'ContactFlag', 'Uin',
-        'StarFriend', 'Statues'):
+          'AttrStatus', 'SnsFlag', 'MemberCount', 'OwnerUin', 'ContactFlag', 'Uin',
+          'StarFriend', 'Statues'):
     friendInfoTemplate[k] = 0
 friendInfoTemplate['MemberList'] = []
 
+
 def clear_screen():
     os.system('cls' if config.OS == 'Windows' else 'clear')
+
 
 def emoji_formatter(d, k):
     ''' _emoji_deebugger is for bugs about emoji match caused by wechat backstage
     like :face with tears of joy: will be replaced with :cat face with tears of joy:
     '''
+
     def _emoji_debugger(d, k):
         s = d[k].replace('<span class="emoji emoji1f450"></span',
-            '<span class="emoji emoji1f450"></span>') # fix missing bug
+                         '<span class="emoji emoji1f450"></span>')  # fix missing bug
+
         def __fix_miss_match(m):
             return '<span class="emoji emoji%s"></span>' % ({
-                '1f63c': '1f601', '1f639': '1f602', '1f63a': '1f603',
-                '1f4ab': '1f616', '1f64d': '1f614', '1f63b': '1f60d',
-                '1f63d': '1f618', '1f64e': '1f621', '1f63f': '1f622',
-                }.get(m.group(1), m.group(1)))
+                                                                '1f63c': '1f601', '1f639': '1f602', '1f63a': '1f603',
+                                                                '1f4ab': '1f616', '1f64d': '1f614', '1f63b': '1f60d',
+                                                                '1f63d': '1f618', '1f64e': '1f621', '1f63f': '1f622',
+                                                            }.get(m.group(1), m.group(1)))
+
         return emojiRegex.sub(__fix_miss_match, s)
+
     def _emoji_formatter(m):
         s = m.group(1)
         if len(s) == 6:
-            return ('\\U%s\\U%s'%(s[:2].rjust(8, '0'), s[2:].rjust(8, '0'))
-                ).encode('utf8').decode('unicode-escape', 'replace')
+            return ('\\U%s\\U%s' % (s[:2].rjust(8, '0'), s[2:].rjust(8, '0'))
+                    ).encode('utf8').decode('unicode-escape', 'replace')
         elif len(s) == 10:
-            return ('\\U%s\\U%s'%(s[:5].rjust(8, '0'), s[5:].rjust(8, '0'))
-                ).encode('utf8').decode('unicode-escape', 'replace')
+            return ('\\U%s\\U%s' % (s[:5].rjust(8, '0'), s[5:].rjust(8, '0'))
+                    ).encode('utf8').decode('unicode-escape', 'replace')
         else:
-            return ('\\U%s'%m.group(1).rjust(8, '0')
-                ).encode('utf8').decode('unicode-escape', 'replace')
+            return ('\\U%s' % m.group(1).rjust(8, '0')
+                    ).encode('utf8').decode('unicode-escape', 'replace')
+
     d[k] = _emoji_debugger(d, k)
     d[k] = emojiRegex.sub(_emoji_formatter, d[k])
+
 
 def msg_formatter(d, k):
     emoji_formatter(d, k)
     d[k] = d[k].replace('<br/>', '\n')
-    d[k]  = htmlParser.unescape(d[k])
+    d[k] = htmlParser.unescape(d[k])
+
 
 def check_file(fileDir):
     try:
@@ -76,6 +85,7 @@ def check_file(fileDir):
     except:
         return False
 
+
 def print_qr(fileDir):
     if config.OS == 'Darwin':
         subprocess.call(['open', fileDir])
@@ -84,6 +94,7 @@ def print_qr(fileDir):
     else:
         os.startfile(fileDir)
 
+
 def print_cmd_qr(qrText, white=BLOCK, black='  ', enableCmdQR=True):
     blockCount = int(enableCmdQR)
     if abs(blockCount) == 0:
@@ -91,16 +102,18 @@ def print_cmd_qr(qrText, white=BLOCK, black='  ', enableCmdQR=True):
     white *= abs(blockCount)
     if blockCount < 0:
         white, black = black, white
-    sys.stdout.write(' '*50 + '\r')
+    sys.stdout.write(' ' * 50 + '\r')
     sys.stdout.flush()
     qr = qrText.replace('0', white).replace('1', black)
     sys.stdout.write(qr)
     sys.stdout.flush()
 
+
 def struct_friend_info(knownInfo):
     member = copy.deepcopy(friendInfoTemplate)
     for k, v in copy.deepcopy(knownInfo).items(): member[k] = v
     return member
+
 
 def search_dict_list(l, key, value):
     ''' Search a list of dict
@@ -109,15 +122,17 @@ def search_dict_list(l, key, value):
         if i.get(key) == value:
             return i
 
-def print_line(msg, oneLine = False):
+
+def print_line(msg, oneLine=False):
     if oneLine:
-        sys.stdout.write(' '*40 + '\r')
+        sys.stdout.write(' ' * 40 + '\r')
         sys.stdout.flush()
     else:
         sys.stdout.write('\n')
     sys.stdout.write(msg.encode(sys.stdin.encoding or 'utf8', 'replace'
-        ).decode(sys.stdin.encoding or 'utf8', 'replace'))
+                                ).decode(sys.stdin.encoding or 'utf8', 'replace'))
     sys.stdout.flush()
+
 
 def test_connect(retryTime=5):
     for i in range(retryTime):
@@ -129,9 +144,11 @@ def test_connect(retryTime=5):
                 logger.error(traceback.format_exc())
                 return False
 
+
 def contact_deep_copy(core, contact):
     with core.storageClass.updateLock:
         return copy.deepcopy(contact)
+
 
 def get_image_postfix(data):
     data = data[:20]
