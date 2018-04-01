0366ca8b0a30853ede84b8c68837a94c0fd6c7ea
Wilson Wang <wilsonwang371@users.noreply.github.com>
2018-03-31 21:15:17 -0700

    update rpc

--- itchat/components/rpc.py
+++ itchat/components/rpc.py
@@ -1,27 +1,28 @@
+import base64
+import io
+import json
 import logging
-import six.moves.xmlrpc_server as rpc
-import six.moves.xmlrpc_client as cli
-import itchat.storage.templates as tpl
-import itchat.returnvalues as rv
-import itchat.storage.messagequeue as mq
 import threading
 import types
-import base64
-import json
+
+import itchat.returnvalues as rv
+import itchat.storage.templates as tpl
+import six.moves.xmlrpc_client as cli
+import six.moves.xmlrpc_server as rpc
 
 logger = logging.getLogger('itchat')
 
 exported_functions = [
     # components.login
-	'login',
-	'get_QRuuid',
-	'get_QR',
-	'check_login',
-	'web_init',
-	'show_mobile_login',
-	'start_receiving',
-	'get_msg',
-	'logout',
+    'login',
+    'get_QRuuid',
+    'get_QR',
+    'check_login',
+    'web_init',
+    'show_mobile_login',
+    'start_receiving',
+    'get_msg',
+    'logout',
 
     # components.contact
     'update_chatroom',
@@ -30,48 +31,54 @@ exported_functions = [
     'get_friends',
     'get_chatrooms',
     'get_mps',
-	'set_alias',
-	'set_pinned',
-	'add_friend',
-	'get_head_img',
-	'create_chatroom',
-	'set_chatroom_name',
-	'delete_member_from_chatroom',
-	'add_member_into_chatroom',
+    'set_alias',
+    'set_pinned',
+    'add_friend',
+    'get_head_img',
+    'create_chatroom',
+    'set_chatroom_name',
+    'delete_member_from_chatroom',
+    'add_member_into_chatroom',
 
-# components.messages
-	'send_raw_msg',
-	'send_msg',
-	'upload_file',
-	'send_file',
-	'send_image',
-	'send_video',
-	'send',
-	'revoke',
+    # components.messages
+    'send_raw_msg',
+    'send_msg',
+    'upload_file',
+    'send_file',
+    'send_image',
+    'send_video',
+    'send',
+    'revoke',
 
-# components.hotreload
-	'dump_login_status',
-	'load_login_status',
+    # components.hotreload
+    'dump_login_status',
+    'load_login_status',
 
-# components.register
-	'auto_login',
-	'configured_reply',
-	'msg_register',
-	'run',
+    # components.register
+    'auto_login',
+    'configured_reply',
+    'msg_register',
+    'run',
 
-# other functions
-	'search_friends',
-	'search_chatrooms',
-	'search_mps',
+    # other functions
+    'search_friends',
+    'search_chatrooms',
+    'search_mps',
     'set_logging',
-	'recv',
+    'recv',
 ]
 
 
 def dump_obj(marshaller, value, write, escape=cli.escape):
-    write('<value><string>')
-    write(escape(json.dumps(value)))
-    write('</string></value>')
+    if isinstance(value, io.BytesIO):
+        write('<value><base64>')
+        encoded = base64.encodebytes(value.getvalue())
+        write(encoded.decode('ascii'))
+        write('</base64></value>')
+    else:
+        write('<value><string>')
+        write(escape(json.dumps(value)))
+        write('</string></value>')
 
 
 def register_types():
@@ -81,7 +88,7 @@ def register_types():
     cli.Marshaller.dispatch[tpl.ChatroomMember] = dump_obj
     cli.Marshaller.dispatch[tpl.MassivePlatform] = dump_obj
     cli.Marshaller.dispatch[rv.ReturnValue] = dump_obj
-    cli.Marshaller.dispatch[mq.Message] = dump_obj
+    cli.Marshaller.dispatch[io.BytesIO] = dump_obj
 
 
 def load_rpc(core):
