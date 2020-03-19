3142501f23aedb05d1e8248b8d63eeb01ffef58a
SuperKenVery <39673849+SuperKenVery@users.noreply.github.com>
2020-03-19 17:15:24 +0800

    Add files via upload

---
+++ itchat/components/__init__.py
@@ -0,0 +1,14 @@
+from .contact import load_contact
+from .hotreload import load_hotreload
+from .login import load_login
+from .messages import load_messages
+from .register import load_register
+from .error import load_error
+
+def load_components(core):
+    load_contact(core)
+    load_hotreload(core)
+    load_login(core)
+    load_messages(core)
+    load_register(core)
+    load_error(core)
